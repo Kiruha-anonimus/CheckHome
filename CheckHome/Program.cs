4546c1b909d565a14c501f0597f9c5936d08305a
Kirill <6392522@mail.ru>
2026-05-12 13:11:50 +0300

    Дебаг

--- CheckHome/Program.cs
+++ CheckHome/Program.cs
@@ -404,7 +404,7 @@ Task HandlePollingErrorAsync(ITelegramBotClient bot, Exception ex, CancellationT
 // Минимальный HTTP-сервер для Render Health Check
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-var httpUrl = $"http://0.0.0.0:{port}";
+var httpUrl = $"http://0.0.0.0:{port}/";
 
 var httpListener = new HttpListener();
 httpListener.Prefixes.Add(httpUrl);
